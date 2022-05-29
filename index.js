ac461df1bd12b517d2448562a854b03701fa8ef8
John Garbero <john.garbero.itb@gmail.com>
2022-05-29 15:07:53 -0500

    commit tree d4cb338c37039c347469f817f6c00e5ad080f5d5

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let split = word.split("")
+  let reverseArray = split.reverse();
+  let drow = reverseArray.join("");
+  //function here to reverse word
+  if (word === drow) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  have a function with a string input()
+    take that string output and reverse it
+    if the reverse is the same as forward, return true
+    else if the reverse is not the same, return false
 */
 
 /*
